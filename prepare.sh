74556c371444779a69e79c7d7ca7df2a5eb85462
tkow <t.kusama0613@outlook.jp>
2022-01-17 05:37:10 +0900

    feat: buildができた

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,6 @@
-!#/usr/bin/bash
+#!/bin/bash
 
-(cd ./react-admin && npx yarn install && npm run build)
+(cd ./react-admin/packages/react-admin && npx yarn link)
+(cd ./react-admin/packages/ra-data-json-server && npx yarn link)
+
+npx yarn link react-admin && npx yarn link ra-data-json-server
